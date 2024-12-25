5a31e4750fac2a243833b94ab34e95d75dfcc390
slava-famcs <16bar2007@gmail.com>
2024-12-25 03:54:46 +0300

    Все сделано

--- task1_string/my_string.cpp
+++ task1_string/my_string.cpp
@@ -3,10 +3,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "my_string.h"
+#include "include/spdlog/spdlog.h"
 #include <cstring>
 #include <stdexcept>
 
-String::String() : size_(0), capacity_(0), string_(nullptr) {}
+String::String() : size_(0), capacity_(0), string_(nullptr) {
+	spdlog::debug("Created an object of type String with size {}, capacity {},\
+		string {}", size_, capacity_, string_);
+}
 
 String::String(const char* str) {
 	if (str) { // если копируемся не от пустой строки
@@ -19,6 +23,8 @@ String::String(const char* str) {
 		size_ = capacity_ = 0;
 		string_ = nullptr;
 	}
+	spdlog::debug("Created an object of type String with size {}, capacity {},\
+		string {}", size_, capacity_, string_);
 }
 
 String::String(const int& count, const char& ch) : size_(count), capacity_(count) {
@@ -26,11 +32,17 @@ String::String(const int& count, const char& ch) : size_(count), capacity_(count
 	for (size_t i = 0; i < count; ++i) {
 		string_[i] = ch;
 	}
+	spdlog::debug("Created an object of type String with size {}, capacity {},\
+		string {}", size_, capacity_, string_);
 }
 
 String::String(String&& other) noexcept : string_(other.string_), size_(other.size_),
 capacity_(other.capacity_) {
-	other = String();
+	other.size_ = 0;
+	other.string_ = nullptr;
+	delete other.string_;
+	spdlog::debug("Moved an object of type String with size {}, capacity {},\
+		string {}", size_, capacity_, string_);
 }
 
 String& String::operator=(String&& other) noexcept {
@@ -42,8 +54,10 @@ String& String::operator=(String&& other) noexcept {
 		capacity_ = other.capacity_;
 		other.string_ = nullptr;
 		delete other.string_;
-		other.size_ = other.capacity_ = 0;
+		other.size_ = 0;
 	}
+	spdlog::debug("Moved an object of type String with size {}, capacity {},\
+		string {}", size_, capacity_, string_);
 	return *this;
 }
 
@@ -51,17 +65,21 @@ String::~String() {
 	size_ = capacity_ = 0;
 	string_ = nullptr;
 	delete string_;
+	spdlog::debug("The object was deleted");
 }
 
 int String::capacity() const {
+	spdlog::debug("capacity() returned {}", (int)capacity_);
 	return (int)capacity_;
 }
 
 int String::length() const {
+	spdlog::debug("length() returned {}", (int)size_);
 	return (int)size_;
 }
 
 bool String::empty() const {
+	spdlog::debug("empty() returned {}", !size_);
 	return !size_;
 }
 
@@ -71,6 +89,7 @@ const char* String::c_str() const {
 		tmp[i] = string_[i];
 	}
 	tmp[size_] = '\0';
+	spdlog::debug("C string is {}", tmp);
 	return tmp;
 }
 
@@ -78,6 +97,7 @@ char& String::operator[](const size_t& index) {
 	if (index >= size_) {
 		throw std::out_of_range("index is outside the array");
 	}
+	spdlog::debug("string[{}] is {}", index, string_[index]);
 	return string_[index];
 }
 
@@ -85,6 +105,7 @@ const char& String::operator[](const size_t& index) const {
 	if (index >= size_) {
 		throw std::out_of_range("index is outside the array");
 	}
+	spdlog::debug("string[{}] is {}", index, string_[index]);
 	return string_[index];
 }
 
@@ -92,6 +113,7 @@ char& String::front() {
 	if (!size_) {
 		throw std::out_of_range("the string is empty");
 	}
+	spdlog::debug("The first element is {}", string_[0]);
 	return string_[0];
 }
 
@@ -99,6 +121,7 @@ const char& String::front() const {
 	if (!size_) {
 		throw std::out_of_range("the string is empty");
 	}
+	spdlog::debug("The first element is {}", string_[0]);
 	return string_[0];
 }
 
@@ -106,6 +129,7 @@ char& String::back() {
 	if (!size_) {
 		throw std::out_of_range("the string is empty");
 	}
+	spdlog::debug("The last element is {}", string_[size_ - 1]);
 	return string_[size_ - 1];
 }
 
@@ -113,6 +137,7 @@ const char& String::back() const {
 	if (!size_) {
 		throw std::out_of_range("the string is empty");
 	}
+	spdlog::debug("The last element is {}", string_[size_ - 1]);
 	return string_[size_ - 1];
 }
 
@@ -126,6 +151,8 @@ void String::reserve(const int& capacity) {
 		delete string_;
 		string_ = tmp;
 		capacity_ = capacity;
+		spdlog::info("{} bytes of memory have been allocated ({} additionally)",
+			capacity, capacity - capacity_);
 	}
 }
 
@@ -135,6 +162,8 @@ void String::push_back(const char& ch) {
 	}
 	string_[size_] = ch;
 	++size_;
+	spdlog::debug("Size is {}", size_);
+	spdlog::info("Symbol {} was added to the string", ch);
 }
 
 void String::pop_back() {
@@ -142,10 +171,14 @@ void String::pop_back() {
 		throw std::out_of_range("the string is empty");
 	}
 	--size_; // посл. симв. остался в памяти, но уже как мусор
+	spdlog::debug("Size is {}", size_);
+	spdlog::info("Symbol {} was removed from the string", string_[size_]);
 }
 
 void String::clear() {
 	size_ = 0;
+	spdlog::debug("Size is {}", size_);
+	spdlog::info("The string was cleared");
 }
 
 void String::insert(const int& index, const String& str) {
@@ -164,6 +197,7 @@ void String::insert(const int& index, const String& str) {
 		string_[index + i] = str.string_[i]; // копируем элементы по индексу
 	}
 	size_ += str.size_;
+	spdlog::info("At position {} was inserted string {}", index, str.c_str());
 }
 
 void String::insert(const int& index, const char* str, const int& count) {
@@ -182,6 +216,8 @@ void String::insert(const int& index, const char* str, const int& count) {
 		string_[index + i] = str[i];
 	}
 	size_ += count;
+	spdlog::info("At position {} was inserted first {} elements of string {}",
+		index, count, str);
 }
 
 void String::erase(int index, int count = 1) {
@@ -194,6 +230,8 @@ void String::erase(int index, int count = 1) {
 		string_[i] = string_[i + count]; // cдвиг влево
 	}
 	size_ -= count;
+	spdlog::info("After the {}th element {} were deleted from the string",
+		index, count);
 }
 
 String operator+(const String& lvalue, const String& rvalue) {
@@ -205,6 +243,8 @@ String operator+(const String& lvalue, const String& rvalue) {
 	for (size_t i = 0; i < rvalue.size_; ++i) {
 		sum.push_back(rvalue.string_[i]);
 	}
+	spdlog::debug("The sum of {} and {} is {}", lvalue.c_str(), rvalue.c_str(),
+		sum.c_str());
 	return sum;
 }
 
@@ -213,6 +253,7 @@ String& String::operator+=(const String other) {
 	for (size_t i = 0; i < other.size_; ++i) {
 		push_back(other.string_[i]);
 	}
+	spdlog::info("To the string was added {}", other.c_str());
 	return *this;
 }
 
